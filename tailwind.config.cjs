a917874053934065972347da21357562ed93f766
Parth <taksuparth@gmail.com>
2023-09-24 05:45:42 +0530

    feat: Add desktop video popover

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require("tailwindcss/defaultTheme");
-
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		fontSize: {
-			...defaultTheme.fontSize,
 			'h1-d': [
 				'56px',
 				{
@@ -60,25 +57,23 @@ module.exports = {
 			'p1-m': ['16px', { lineHeight: '20px', }],
 			'p2-m': ['14px', { lineHeight: '20px', }],
 		},
-		colors: {
-			...defaultTheme.colors,
-			deepBlack: '#16160e',
-			deepWhite: '#FFFFFF',
-			chalk: '#FFFFFC',
-			dough: '#fbfaf5',
-			greyYellowGreen: '#e6eae3',
-			buckwheaT: '#d4dcd6',
-			sageGreen: '#97a791',
-			willowGrey: '#c8d5bb',
-			greySilk: '#dddcd6',
-			moisture: '#c8c2be'
-		},
-		fontFamily: {
-			...defaultTheme.fontFamily,
-			grotesk: ['Space Grotesk', 'sans-serif'],
-			inter: ['Inter', 'sans-serif']
-		},
 		extend: {
+			colors: {
+				deepBlack: '#16160e',
+				deepWhite: '#FFFFFF',
+				chalk: '#FFFFFC',
+				dough: '#fbfaf5',
+				greyYellowGreen: '#e6eae3',
+				buckwheaT: '#d4dcd6',
+				sageGreen: '#97a791',
+				willowGrey: '#c8d5bb',
+				greySilk: '#dddcd6',
+				moisture: '#c8c2be'
+			},
+			fontFamily: {
+				grotesk: ['Space Grotesk', 'sans-serif'],
+				inter: ['Inter', 'sans-serif']
+			},
 			padding: {
 				18: '4.5rem'
 			},
